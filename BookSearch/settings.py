b275313ff70fa40e1883d0bad0cb82ab428d2c95
OmarIbrahim991 <omar.ibrahim.991@gmail.com>
2019-03-30 08:33:18 +0200

    Created authentication app

--- BookSearch/settings.py
+++ BookSearch/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
 	'main.apps.MainConfig',
+	'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
